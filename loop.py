53fd30c0b51651e8a4869c555f87953633b85e83
Albert Richards <ARichards@qa.com>
2021-06-04 14:16:05 +0000

    iteration and collection exercises

---
+++ loop.py
@@ -0,0 +1,8 @@
+count = 0
+while count < 5:
+    name = str(input(f"What is the name of person {count +1}?"))
+    print(f"{name} is awesome!")
+    count += 1
+
+for i in range(10,21,2):
+    print(i)
